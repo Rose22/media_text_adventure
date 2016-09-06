e7c8187969146fb719fbc38769abbd43cc1c49df
Rose <Rose22@users.noreply.github.com>
2016-09-06 11:45:15 +0200

    Added GPL

--- MediaTextAdventure.py
+++ MediaTextAdventure.py
@@ -14,7 +14,21 @@ import sys, time, os, pygame
 # Creators can assign custom sounds to every action, by replacing the standard sounds in "sound/" with their own.
 # To add background music for a particular area, all the creator needs to do is place a music file in sound/ with the name of the location. For example, "road.mp3"
 
-# By Rose22
+#########
+# Copyright (c) 2016 Rose (Rose22)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ######
 # Function definitions
